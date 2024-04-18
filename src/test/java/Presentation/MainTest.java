bf4d6e428479f6693fd35ffaee4b91f146d4db2d
rhit-gaultzt <gaultzt@rose-hulman.edu>
2024-04-18 15:40:37 -0400

    Added tests for Main.findIssues

--- src/test/java/Presentation/MainTest.java
+++ src/test/java/Presentation/MainTest.java
@@ -2,6 +2,7 @@ package Presentation;
 
 import Data.JavaByteCodeAdapter.ASM.ClassReaderASM;
 import Data.JavaByteCodeAdapter.ClassNode;
+import Data.JavaByteCodeAdapter.ClassReader;
 import Data.OptionsReaderYAML;
 import Domain.ClassStreamHandler;
 import Domain.Issue;
@@ -14,7 +15,9 @@ import org.junit.jupiter.api.Assertions;
 import java.io.*;
 import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainTest {
 
@@ -106,5 +109,76 @@ public class MainTest {
 
         // Verify
         EasyMock.verify(ruleHandler, changeJarOutput);
+    }
+
+    @Test
+    public void testFindIssuesNoneCharacterization() throws IOException {
+        // Record
+        Main main = new Main();
+        String[] args = {"a.class"};
+        OptionsReaderYAML optionsReader = EasyMock.mock(OptionsReaderYAML.class);
+        ChangeJarOutput jarOuput = EasyMock.mock(ChangeJarOutput.class);
+        CLIGetClasses getClasses = EasyMock.mock(CLIGetClasses.class);
+        ClassReader classReader = EasyMock.mock(ClassReader.class);
+        ClassStreamHandler streamHandler = EasyMock.mock(ClassStreamHandler.class);
+
+        EasyMock.expect(getClasses.getClasses(args)).andReturn(args);
+        EasyMock.expect(streamHandler.getClassStreams(args)).andReturn(new HashMap<>());
+        EasyMock.expect(optionsReader.readOptions()).andReturn(new HashMap<>());
+
+        // Replay
+        EasyMock.replay(optionsReader, jarOuput, getClasses, classReader, streamHandler);
+        List<Issue> issues = main.findIssues(args, optionsReader, jarOuput,
+                getClasses, classReader, streamHandler);
+
+        // Verify
+        EasyMock.verify(optionsReader, jarOuput, getClasses, classReader, streamHandler);
+        Assertions.assertEquals(0, issues.size());
+    }
+
+    @Test
+    public void testFindIssuesWithClassesCharacterization() throws IOException {
+        // Record
+        Main main = new Main();
+        String[] args = {"a.class"};
+        OptionsReaderYAML optionsReader = EasyMock.mock(OptionsReaderYAML.class);
+        ChangeJarOutput jarOuput = EasyMock.mock(ChangeJarOutput.class);
+        CLIGetClasses getClasses = EasyMock.mock(CLIGetClasses.class);
+        ClassReader classReader = EasyMock.mock(ClassReader.class);
+        ClassStreamHandler streamHandler = EasyMock.mock(ClassStreamHandler.class);
+        InputStream stream = EasyMock.mock(InputStream.class);
+        ClassNode node = EasyMock.mock(ClassNode.class);
+
+        HashMap<String, InputStream> streams = new HashMap<>();
+        streams.put(args[0], stream);
+
+        EasyMock.expect(getClasses.getClasses(args)).andReturn(args);
+        EasyMock.expect(streamHandler.getClassStreams(args)).andReturn(streams);
+        EasyMock.expect(optionsReader.readOptions()).andReturn(new HashMap<>());
+        EasyMock.expect(classReader.createClassNode(stream)).andReturn(node);
+        stream.close();
+        EasyMock.expectLastCall();
+        EasyMock.expect(node.getSourceFile()).andReturn("c.class").times(3);
+        EasyMock.expect(node.isAbstract()).andReturn(false);
+        EasyMock.expect(node.getSuperClass()).andReturn(node);
+        EasyMock.expect(node.getClassName()).andReturn("c").times(6);
+        EasyMock.expect(node.getFields()).andReturn(new ArrayList<>()).times(4);
+        EasyMock.expect(node.getMethods()).andReturn(new ArrayList<>()).times(7);
+        EasyMock.expect(node.isValid()).andReturn(true);
+        EasyMock.expect(node.getFileName()).andReturn("c.class").times(2);
+        EasyMock.expect(node.getInterfaces()).andReturn(new ArrayList<>());
+        EasyMock.expect(node.getShortenedClassName()).andReturn("c");
+
+        // Replay
+        EasyMock.replay(optionsReader, jarOuput, getClasses, classReader, streamHandler, stream, node);
+        List<Issue> issues = main.findIssues(args, optionsReader, jarOuput,
+                getClasses, classReader, streamHandler);
+
+        // Verify
+        EasyMock.verify(optionsReader, jarOuput, getClasses, classReader, streamHandler, stream, node);
+        Assertions.assertEquals(1, issues.size());
+        Assertions.assertEquals("ClassNameRule", issues.get(0).getRule());
+        Assertions.assertEquals("class c does not begin with a capital letter", issues.get(0).getMessage());
+        Assertions.assertEquals(-1, issues.get(0).getLine());
     }
 }
